454b8667ded5fef698d09033d0c535d9bb8820d0
Dewansh Rawat <dewanshrawat15@gmail.com>
2018-09-25 14:30:19 +0530

    Added About and Timeline

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){q
+jQuery(document).ready(function(){
 
 	setInterval(function(){
 		$('.scroll-link').addClass('neeche')
@@ -21,13 +21,17 @@ jQuery(document).ready(function(){q
     $('.hero').css('height', ''+htemp+'');
 	})
 
-
 	$('.scroll-link').click(function(){
 		$('html, body').animate({ scrollTop: $('.about').offset().top}, 750)
 	})
 
 	$('.author').click(function(){
 		window.open("https://github.com/EDCBVUCOEP")
+	})
+
+	/*When clicking on Full hide fail/success boxes */
+	$('#name').focus(function() {
+  		$('#success').html('');
 	})
 
 })
